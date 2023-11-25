92a70b8e699fb30d88163365ff06c2fa9f0cb4a1
AmineNekhla <124872758+AmineNekhla@users.noreply.github.com>
2023-11-25 22:54:29 +0100

    Update login.php

---
+++ TPs/TP06-forum/views/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body class="container">
+    <div style="width: 40%; display:grid; margin:auto;">
+    <h2 class="mt-5">Connexion</h2>
+    <form action="../controller\auth.php" method="post" class="mt-4" >
+        <div class="mb-3">
+            <label for="email" class="form-label">Email :</label>
+            <input type="email" name="email" class="form-control" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="password" class="form-label">Mot de passe :</label>
+            <input type="password" name="password" class="form-control" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
